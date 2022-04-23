febd67d39077f964451a3906bd735f6fde21a23f
danjae98 <numbness98@naver.com>
2022-04-23 11:49:27 +0900

    출력 형성평가

---
+++ jungol/출력/1_test.py
@@ -0,0 +1,16 @@
+print("My name is Hong")
+
+print("My hometown")
+print("Flowering mountain")
+
+print("TTTTTTTTTT")
+print("TTTTTTTTTT")
+print("    TT    ")
+print("    TT    ")
+print("    TT    ")
+
+print("kor 90")
+print("mat 80")
+print("eng 100")
+print("sum",str(90 + 80 +100))
+print("avg",int((90+80+100)/3))
